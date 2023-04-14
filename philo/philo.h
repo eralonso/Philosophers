ef553f97d098789a021bbd4e2229c9a877ee88fa
Eric Alonso Martinez <eralonso@car9s3.42barcelona.com>
2023-04-14 14:01:08 +0200

    Starting Philo

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 12:45:10 by eralonso          #+#    #+#             */
+/*   Updated: 2023/04/14 13:44:32 by eralonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include	<stdlib.h>
+# include	<stdio.h>
+# include	<unistd.h>
+# include	<pthread.h>
+# include	<sys/time.h>
+
+#endif
