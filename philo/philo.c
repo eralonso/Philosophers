16ff34417d3a62d164626447a0d572f0f54f6b29
Eric Alonso Martinez <eralonso@car15s1.42barcelona.com>
2023-04-16 19:28:04 +0200

    Starting philosophing

--- philo/philo.c
+++ philo/philo.c
@@ -6,20 +6,79 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 12:44:29 by eralonso          #+#    #+#             */
-/*   Updated: 2023/04/14 13:42:15 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/04/16 19:27:37 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<philo.h>
 
+void	*rutine(void *table)
+{
+	t_table	*cast_table;
+	
+	cast_table = (t_table *)table;
+	while (!cast_table->any_dead)
+	{
+		printf("a");
+	}
+	
+}
+
 int	main(int ac, char ** av)
 {
-	int	i;
+	t_table	table;
+	int		err;
+	int		n_philo;
+	int		i;
 
-	if (ac <= 4 || ac > 6)
+	(void)	ac;
+	(void)	av;
+	// if (ac <)
+	if (ac != 2)
 		return (1);
-	i = 0;
-	while(++i < ac)
-		printf("%s\n", av[i]);
+	n_philo = atoi(av[1]);
+	if (!n_philo)
+		return (1);
+	table.any_dead = 0;
+	table.philos = (t_philo *)malloc(sizeof(t_philo) * n_philo);
+	if (!table.philos)
+		return (1);
+	table.forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * n_philo);
+	if (!table.forks)
+	{
+		free(table.philos);
+		return (1);
+	}
+	if (pthread_mutex_init(&table.print, NULL))
+	{
+		free(table.philos);
+		free(table.forks);
+		return (1);
+	}
+	i = -1;
+	while (++i < n_philo)
+	{
+		if (pthread_mutex_init(&table.forks[i], NULL))
+		{
+			free(table.philos);
+			free(table.forks);
+			pthread_mutex_destroy(&table.print);
+			return (1);
+		}
+	}
+	i = -1;
+	while (++i < n_philo)
+	{
+		table.philos[i].n = i + 1;
+		table.philos[i].alive = 1;
+		table.philos[i].r_fork = table.forks[i];
+		table.philos[i].l_fork = table.forks[(i + 1) % n_philo];
+	}
+	err = pthread_create(&table.philos[i].id, NULL, rutine, &table);
+	if (err)
+	{
+		dprintf(2, "Error \'pthread_create()\'.\nThread %i", i);
+		return (1);
+	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 12:45:10 by eralonso          #+#    #+#             */
-/*   Updated: 2023/04/14 13:44:32 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/04/16 19:18:04 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,24 @@
 # include	<unistd.h>
 # include	<pthread.h>
 # include	<sys/time.h>
+
+typedef struct s_table	t_table;
+typedef struct s_philo	t_philo;
+
+struct s_table {
+	pthread_mutex_t	print;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+	int				any_dead;
+};
+
+struct s_philo
+{
+	pthread_t		id;
+	int				alive;
+	int				n;
+	pthread_mutex_t	r_fork;
+	pthread_mutex_t	l_fork;
+};
 
 #endif
