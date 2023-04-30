12174914fcc6f289c738993a42daf7d5b05cadb4
Eric Alonso Martinez <eralonso@car15s1.42barcelona.com>
2023-04-30 14:16:54 +0200

    more bash

--- philo/script.sh
+++ philo/script.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-if [[ $# < 6 || $# > 7 ]]; then
+argc=$#
+argv=($@)
+
+if [[ ${argv[0]} == "-h" || ${argv[0]} == "--help" ]]; then
+	echo -e "\033[1;92mUsage: \033[1;94m./script.sh <options: [--help/-h...]> N_LOOPS T_DELAY N_PHILO TIME_TO_DIE TIME_TO_EAT TIME_TO_SLEEP <N_TIMES_EAT>\n\033[0m"
+	echo -e "\033[1;93mOptions:"
+	echo -e "-cd1: Check Death - level 1"
+	echo -e "-cd2: Check Death - level 2"
+	echo -e "-cda: Check Death - all levels"
+	exit 1;
+fi
+
+level=0
+
+if [ ${argv[0]} == "-cd1" ]; then
+	level=1
+elif [ ${argv[0]} == "-cd2" ]; then
+	level=2
+fi
+
+if [[ ${argv[0]} == "-cd1" || ${argv[0]} == "-cd2" || ${argv[0]} == "-cda" ]]; then
+	((argc--))
+	unset argv[0]
+	argv=(${argv[@]})
+fi
+
+if [[ $argc < 6 || $argc > 7 ]]; then
 	echo ""
 	echo -e "\033[1;93mInvalid number of arguments\033[0m"
 	echo ""
@@ -55,55 +81,58 @@ function 	all_philos_time_analyzer(){
 
 function dead_time_analyzer()
 {
-	# if [ $ ]; then
-	echo -e "\n\033[1mTest 1 - \033[1;93mCheck the two last meals (of the philo dead) and the dead time:\033[0m"
-	PHILOS_DIED=$(echo -e "$OUT" | grep "died" | wc -l)
-	if [ $PHILOS_DIED -gt 1 ]; then
-		echo "\t\033[1;91mKO: More than 1 philo died -> ($PHILOS_DIED)\033[0m"
-		return ;
-	fi
-	POST_DEAD=$(echo -e "$OUT" | tail -1 | awk 'BEGIN {res=0} $3 != "died" {res=1} END {print res}')
-	if [ $POST_DEAD -eq 1 ]; then
-		echo -e "\t\033[1;91mKO\033[1;37m: There are messages after the death of a philo!!!\033[0m"
-		return ;
-	fi
-	WEIRD_TIME=$(echo -e "$OUT" | awk 'BEGIN {weird=0} $1 < 0 {weird=-1} END {print weird}')
-	if [ $WEIRD_TIME == -1 ]; then
-		echo -e "\t\033[1;91mKO\033[1;37m: Fatal error with the time:\n\t\033[1;93mIS NEGATIVE WTF 😵!!!\033[0m"
-		return ;
-	fi
-	INIT_TIME=$(echo -e "$OUT" | head -1 | awk '{print $1}')
-	INIT_EAT_TIME=$(echo -e "$OUT" | grep "eating" | head -1 | awk '{print $1}')
-	PHILO=$(echo -e "$OUT" | grep "died" | awk '{print $2}')
-	TIMES_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -2 | wc -l)
-	DEAD_TIME=$(echo -e "$OUT" | grep " $PHILO died" | awk '{print $1}')
-	LT_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -1 | awk '{print $1}')
-	if [ $TIMES_EAT -gt 1 ]; then
-		PN_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -2 | head -1 | awk '{print $1}')
-		if [[ $((DEAD_TIME - $LT_EAT)) -gt $((T_DIE + 10)) ]]; then
-			echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Penultimate eat: $PN_EAT; Ultimate eat: $LT_EAT: Time could be die: $((LT_EAT + $T_DIE))"
-		elif [[ $((DEAD_TIME - $PN_EAT)) -gt $((T_DIE + 10)) ]]; then
-			echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Penultimate eat: $PN_EAT; Ultimate eat: $LT_EAT: Time could be die: $((PN_EAT + $T_DIE))"
-		else
-			echo -e "\t\033[1;92mOK\033[0m"
+	if [[ $level -eq 0 || $level -eq 1 ]]; then
+		echo -e "\n\033[1mTest 1 - \033[1;93mCheck the two last meals (of the philo dead) and the dead time:\033[0m"
+		PHILOS_DIED=$(echo -e "$OUT" | grep "died" | wc -l)
+		if [ $PHILOS_DIED -gt 1 ]; then
+			echo "\t\033[1;91mKO: More than 1 philo died -> ($PHILOS_DIED)\033[0m"
+			return ;
 		fi
-	elif [ $TIMES_EAT -lt 1 ]; then
-		if [[ $DEAD_TIME -gt $((T_DIE + 10)) ]]; then
-			echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Time could be die: $T_DIE"
-		else
-			echo -e "\t\033[1;92mOK\033[0m"
+		POST_DEAD=$(echo -e "$OUT" | tail -1 | awk 'BEGIN {res=0} $3 != "died" {res=1} END {print res}')
+		if [ $POST_DEAD -eq 1 ]; then
+			echo -e "\t\033[1;91mKO\033[1;37m: There are messages after the death of a philo!!!\033[0m"
+			return ;
 		fi
-	else
-		if [[ $((DEAD_TIME - $LT_EAT)) -gt $((T_DIE + 10 + $INIT_TIME)) ]]; then
-			echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Ultimate eat: $LT_EAT: Time could be die: $((LT_EAT + $T_DIE))"
-		elif [[ $DEAD_TIME -gt $((T_DIE + 10)) ]]; then
-			echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Time could be die: $T_DIE"
+		WEIRD_TIME=$(echo -e "$OUT" | awk 'BEGIN {weird=0} $1 < 0 {weird=-1} END {print weird}')
+		if [ $WEIRD_TIME == -1 ]; then
+			echo -e "\t\033[1;91mKO\033[1;37m: Fatal error with the time:\n\t\033[1;93mIS NEGATIVE WTF 😵!!!\033[0m"
+			return ;
+		fi
+		INIT_TIME=$(echo -e "$OUT" | head -1 | awk '{print $1}')
+		INIT_EAT_TIME=$(echo -e "$OUT" | grep "eating" | head -1 | awk '{print $1}')
+		PHILO=$(echo -e "$OUT" | grep "died" | awk '{print $2}')
+		TIMES_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -2 | wc -l)
+		DEAD_TIME=$(echo -e "$OUT" | grep " $PHILO died" | awk '{print $1}')
+		LT_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -1 | awk '{print $1}')
+		if [ $TIMES_EAT -gt 1 ]; then
+			PN_EAT=$(echo -e "$OUT" | grep " $PHILO is eating" | tail -2 | head -1 | awk '{print $1}')
+			if [[ $((DEAD_TIME - $LT_EAT)) -gt $((T_DIE + 10)) ]]; then
+				echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Penultimate eat: $PN_EAT; Ultimate eat: $LT_EAT: Time could be die: $((LT_EAT + $T_DIE))"
+			elif [[ $((DEAD_TIME - $PN_EAT)) -gt $((T_DIE + 10)) ]]; then
+				echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Penultimate eat: $PN_EAT; Ultimate eat: $LT_EAT: Time could be die: $((PN_EAT + $T_DIE))"
+			else
+				echo -e "\t\033[1;92mOK\033[0m"
+			fi
+		elif [ $TIMES_EAT -lt 1 ]; then
+			if [[ $DEAD_TIME -gt $((T_DIE + 10)) ]]; then
+				echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Time could be die: $T_DIE"
+			else
+				echo -e "\t\033[1;92mOK\033[0m"
+			fi
 		else
-			echo -e "\t\033[1;92mOK\033[0m"
+			if [[ $((DEAD_TIME - $LT_EAT)) -gt $((T_DIE + 10 + $INIT_TIME)) ]]; then
+				echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Ultimate eat: $LT_EAT: Time could be die: $((LT_EAT + $T_DIE))"
+			elif [[ $DEAD_TIME -gt $((T_DIE + 10)) ]]; then
+				echo -e "\t\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Time could be die: $T_DIE"
+			else
+				echo -e "\t\033[1;92mOK\033[0m"
+			fi
 		fi
 	fi
-	echo -e "\n\033[1mTest 2 - \033[1;93mCheck all meals of all philos - (Maybe the result is equal or similarity):\033[0m"
-	all_philos_time_analyzer
+	if [[ $level -eq 0 || $level -eq 2 ]]; then
+		echo -e "\n\033[1mTest 2 - \033[1;93mCheck all meals of all philos - (Maybe the result is equal or similarity):\033[0m"
+		all_philos_time_analyzer
+	fi
 
 	# if [[ $((LT_EAT - $PN_EAT)) > $((T_DIE + 10)) ]]; then
 	# 	echo -e "\033[1;91mKO\033[1;37m: Bad dead time with philo \033[1;94m$PHILO\033[1;37m: Die after could be die -> Penultimate eat: $PN_EAT; Ultimate eat: $PN_EAT: Time could be die: $((PN_EAT + $T_DIE))"
@@ -149,29 +178,21 @@ function dead_time_analyzer()
 	return ;
 }
 
-if [[ $1 == "-h" || $1 == "--help" ]]; then
-	echo -e "\033[1;92mUsage: \033[1;94m./script.sh <options: [--help/-h...]> N_LOOPS T_DELAY N_PHILO TIME_TO_DIE TIME_TO_EAT TIME_TO_SLEEP <N_TIMES_EAT>\033[0m"
-	echo -e "\033[1;94mOptions:"
-	echo -e "-cd1: Check Death - level 1"
-	echo -e "-cd2: Check Death - level 2"
-	echo -e "-cda: Check Death - all levels"
-	exit 1;
-fi
 
-LOOPS=$1
-T_DELAY=$2
-N_PHILO=$3
-T_DIE=$4
-T_EAT=$5
-T_SLP=$6
-if [ $# == 7 ]; then
-	TT_EAT=$7
+LOOPS=${argv[0]}
+T_DELAY=${argv[1]}
+N_PHILO=${argv[2]}
+T_DIE=${argv[3]}
+T_EAT=${argv[4]}
+T_SLP=${argv[5]}
+if [ $argc == 7 ]; then
+	TT_EAT=${argv[6]}
 else
 	TT_EAT=""
 fi
 i=0
 
-while [ $i -lt $LOOPS ]; do
+while [[ $i < $LOOPS ]]; do
 	echo -e "\n\t\033[1;94mLOOP $((i + 1))\033[1;37m\n"
 	OUT=$(./philo $N_PHILO $T_DIE $T_EAT $T_SLP $TT_EAT)
 	echo -e "$OUT"
